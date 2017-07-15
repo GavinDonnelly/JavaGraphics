2eaa053a9adc7ae32926385f28b1289f5879b5d1
Gavin Donnelly <gavin.donnelly0@gmail.com>
2017-07-15 22:17:38 +0100

    Add Mockito tests

--- src/test/java/com/gavindonnelly/g2d/G2DObjectTest.java
+++ src/test/java/com/gavindonnelly/g2d/G2DObjectTest.java
@@ -4,12 +4,16 @@ package com.gavindonnelly.g2d;
  * Created by gavindonnelly on 15/07/2017.
  */
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class G2DObjectTest {
+
     @Test
-    public void evaluatesExpression() {
-        assertEquals(6, 6);
+    public void shouldFailOnNullIdentifiers() {
+        G2DObject g2dObjectMockEmpty = Mockito.mock(G2DObject.class, Mockito.CALLS_REAL_METHODS);
+        assertNull(g2dObjectMockEmpty.getColor());
     }
+
 }
