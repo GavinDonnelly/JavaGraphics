ed9552a5f01ae3c782a51a522ed31798f0c98c80
Gavin Donnelly <gavin.donnelly0@gmail.com>
2017-07-15 14:25:07 +0100

    Add test

---
+++ src/test/com/gavindonnelly/g2d/G2DObjectTest.java
@@ -0,0 +1,15 @@
+package com.gavindonnelly.g2d;
+
+/**
+ * Created by gavindonnelly on 15/07/2017.
+ */
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class G2DObjectTest {
+    @Test
+    public void evaluatesExpression() {
+        assertEquals(6, 6);
+    }
+}
